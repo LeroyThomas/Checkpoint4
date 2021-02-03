f2aa0e4e3c0614c0104bfe97b2e1fe85a0465646
Thomas Leroy <thomasleroy50@gmail.com>
2021-02-03 18:01:01 +0100

    login beginning

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,16 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'last_username' => $lastUsername,
+            'error' => $error
         ]);
     }
 }
